1485d6b362db339dcd13f896c2e2d6a9a1765799
Hangbokie <sungkisa@naver.com>
2015-09-04 16:32:20 +0900

    Add jeju.sh
    Script has 1 function.
    root/Unroot

---
+++ jeju.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+### 2015 JEJU PROJECT
+
+#INIT folder
+mkdir /sdcard/jeju
+#Getting Props
+JEJU_HOME=/sdcard/jeju
+
+
+
+if [ -e JEJU_HOME/jeju.root]; then
+rm -f /system/xbin/su
+cp /system/jeju/su /system/xbin/su
+fi
+
+if [ -e JEJU_HOME/jeju.unroot]; then
+rm -f /system/xbin/su
+fi
